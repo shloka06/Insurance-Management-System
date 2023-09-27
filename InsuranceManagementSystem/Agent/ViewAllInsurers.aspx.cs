053e488dd88957805f017c0a32bd635924f22c0f
Shloka Jaywant <shlokajaywant@gmail.com>
2023-09-27 23:09:34 +0530

    Refactored ViewAll Modules

--- InsuranceManagementSystem/Agent/ViewAllInsurers.aspx.cs
+++ InsuranceManagementSystem/Agent/ViewAllInsurers.aspx.cs
@@ -9,29 +9,20 @@ namespace InsuranceManagementSystem.Agent
         Commonfnx fn = new Commonfnx();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (IsPostBack)
-            {
-                GetClass();
-            }
-        }
 
-        private void GetClass()
-        {
-            DataTable dt = fn.Fetch("SELECT INS_ID AS \"Insurer ID\", Ins_Name AS \"Insurer Name\", TRY_CONVERT(date, DOB) AS \"Date Of Birth\", House_Num + ', ' + Street + ', ' + Area + ', ' + City + ' - ' + CONVERT(nchar, Pincode) as \"Address\" FROM INSURER;");
-            GridView1.DataSource = dt;
-            GridView1.DataBind();
         }
 
         protected void btnView_Click(object sender, EventArgs e)
         {
             try
             {
-
+                DataTable dt = fn.Fetch("SELECT INS_ID AS \"Insurer ID\", Ins_Name AS \"Insurer Name\", TRY_CONVERT(date, DOB) AS \"Date Of Birth\", House_Num + ', ' + Street + ', ' + Area + ', ' + City + ' - ' + CONVERT(nchar, Pincode) as \"Address\" FROM INSURER;");
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
             }
             catch (Exception ex)
             {
-
-                throw;
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
             }
         }
     }
--- InsuranceManagementSystem/Agent/ViewAllPolicies.aspx.cs
+++ InsuranceManagementSystem/Agent/ViewAllPolicies.aspx.cs
@@ -14,36 +14,28 @@ namespace InsuranceManagementSystem.Agent
         Commonfnx fn = new Commonfnx();
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (IsPostBack) 
-            {
-                GetClass();
-            }
-        }
 
-        private void GetClass() {
-            DataTable dt = fn.Fetch("Select * from POLICY");
-            dt.Columns["POL_ID"].ColumnName = "Policy ID";
-            dt.Columns["Policy_Name"].ColumnName = "Policy Name";
-            dt.Columns["Policy_Type"].ColumnName = "Type";
-            dt.Columns["Insured_Amount"].ColumnName = "Insured Amount";
-            dt.Columns["Payment_Schedule"].ColumnName = "Payment Schedule";
-            dt.Columns["Payment_Amount"].ColumnName = "Payment Amount";
-            dt.Columns["Payment_Duration"].ColumnName = "Payment Duration";
-            dt.Columns["Cover_Duration"].ColumnName = "Cover Duration";
-            GridView1.DataSource = dt;
-            GridView1.DataBind();
         }
 
         protected void btnView_Click(object sender, EventArgs e)
         {
             try
             {
-
+                DataTable dt = fn.Fetch("Select * from POLICY");
+                dt.Columns["POL_ID"].ColumnName = "Policy ID";
+                dt.Columns["Policy_Name"].ColumnName = "Policy Name";
+                dt.Columns["Policy_Type"].ColumnName = "Type";
+                dt.Columns["Insured_Amount"].ColumnName = "Insured Amount";
+                dt.Columns["Payment_Schedule"].ColumnName = "Payment Schedule";
+                dt.Columns["Payment_Amount"].ColumnName = "Payment Amount";
+                dt.Columns["Payment_Duration"].ColumnName = "Payment Duration";
+                dt.Columns["Cover_Duration"].ColumnName = "Cover Duration";
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
             }
             catch (Exception ex)
             {
-
-                throw;
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
             }
         }
     }
