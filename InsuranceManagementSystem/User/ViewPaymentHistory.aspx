a14805d152ab75e5ce70cdaf09a0fbe22a969d62
Shloka Jaywant <shlokajaywant@gmail.com>
2023-09-30 18:00:44 +0530

    Added View Payment History Module for User

---
+++ InsuranceManagementSystem/User/ViewPaymentHistory.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User/UserMst.Master" AutoEventWireup="true"
+    CodeBehind="ViewPaymentHistory.aspx.cs" Inherits="InsuranceManagementSystem.User.ViewPaymentHistory" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="container p-md-4 p-sm-4">
+        <div>
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+        </div>
+        <h3 class="text-center">VIEW PAYMENTS HISTORY</h3>
+
+        <div class="row mb-3 mr-lg-5 ml-lg-5">
+            <div class="col-md-3 col-md-offset-2 mb-3">
+                <asp:Button ID="btnView" runat="server" CssClass="btn btn-primary btn-block" BackColor="#5558C9"
+                    Text="View Payments History" OnClick="btnView_Click" />
+            </div>
+        </div>
+
+        <div class="row mb-3 mr-lg-5 ml-lg-5">
+            <div class="col-md-6">
+                <asp:GridView ID="GridView1" runat="server" CssClass="table table-hover table-bordered">
+                </asp:GridView>
+            </div>
+        </div>
+
+    </div>
+    </div>
+
+</asp:Content>
---
+++ InsuranceManagementSystem/User/ViewPaymentHistory.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static InsuranceManagementSystem.Models.CommonFn;
+
+namespace InsuranceManagementSystem.User
+{
+    public partial class ViewPaymentHistory : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (CurrentSession.currentSession.SessionName != "User" || CurrentSession.currentSession.SessionID == 0)
+            {
+                Response.Redirect("../Login.aspx");
+            }
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int insID = CurrentSession.currentSession.SessionID;
+
+                DataTable dt = fn.Fetch("SELECT P.PAY_ID AS \"Payment ID\", P.Amount AS \"Payment Amount\", POL.POL_ID AS \"Policy ID\", POL.Policy_Name AS \"Policy Name\", P.Due_Date AS \"Due Date\", P.Date_Paid AS \"Date Paid\", P.Transaction_Status AS \"Transaction Status\" FROM PAYMENT_DETAILS AS P, PAYMENT_RECORD AS PR, POLICY AS POL WHERE P.PAY_ID = PR.PAY_ID AND PR.POL_ID = POL.POL_ID AND PR.INS_ID = " + insID + ";");
+
+                if (dt.Rows.Count > 0)
+                {
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                else
+                {
+                    lblMsg.Text = "No Payment History Found!";
+                    lblMsg.CssClass = "alert alert-danger";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+    }
+}
