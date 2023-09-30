bb98db637f353e9ac97bf9047a693e0e6d844493
Shloka Jaywant <shlokajaywant@gmail.com>
2023-09-30 08:50:17 +0530

    Added skeleton for View Purchased Policies (User) Module

---
+++ InsuranceManagementSystem/User/ViewPurchasedPolicies.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static InsuranceManagementSystem.Models.CommonFn;
+
+namespace InsuranceManagementSystem.User
+{
+    public partial class ViewPurchasedPolicies : System.Web.UI.Page
+    {
+        Commonfnx fn = new Commonfnx();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
