82cda96f5091141d288c90bc9f9034992ee23fb9
Shloka Jaywant <shlokajaywant@gmail.com>
2023-10-15 15:31:41 +0530

    Created Class Policy

---
+++ InsuranceManagementSystem/Agent Classes/Policy.cs
@@ -0,0 +1,34 @@
+﻿namespace InsuranceManagementSystem.Agent_Classes
+{
+    public class Policy
+    {
+        public string PolicyName
+        {
+            get; set;
+        }
+        public string PolicyType
+        {
+            get; set;
+        }
+        public int InsuredAmount
+        {
+            get; set;
+        }
+        public string PaymentSchedule
+        {
+            get; set;
+        }
+        public int PaymentAmount
+        {
+            get; set;
+        }
+        public int PaymentDuration
+        {
+            get; set;
+        }
+        public int CoverDuration
+        {
+            get; set;
+        }
+    }
+}
