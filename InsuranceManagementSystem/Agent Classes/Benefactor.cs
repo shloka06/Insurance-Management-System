ccec43d70ff1b9254141b1c77078b200c84d4d9c
Shloka Jaywant <shlokajaywant@gmail.com>
2023-10-15 12:23:02 +0530

    Added Classes For Insurer, Benefactor

---
+++ InsuranceManagementSystem/Agent Classes/Benefactor.cs
@@ -0,0 +1,30 @@
+﻿namespace InsuranceManagementSystem.Agent_Classes
+{
+    public class Benefactor
+    {
+        public string FirstName
+        {
+            get; set;
+        }
+        public string LastName
+        {
+            get; set;
+        }
+        public int DOB_Day
+        {
+            get; set;
+        }
+        public int DOB_Mon
+        {
+            get; set;
+        }
+        public int DOB_Year
+        {
+            get; set;
+        }
+        public string Relationship
+        {
+            get; set;
+        }
+    }
+}
---
+++ InsuranceManagementSystem/Agent Classes/Insurer.cs
@@ -0,0 +1,66 @@
+﻿namespace InsuranceManagementSystem.Agent_Classes
+{
+    public class Insurer
+    {
+        public string FirstName
+        {
+            get; set;
+        }
+        public string LastName
+        {
+            get; set;
+        }
+        public int DOB_Day
+        {
+            get; set;
+        }
+        public int DOB_Mon
+        {
+            get; set;
+        }
+        public int DOB_Year
+        {
+            get; set;
+        }
+        public string PhNo1
+        {
+            get; set;
+        }
+        public string PhNo2
+        {
+            get; set;
+        }
+        public string PhNo3
+        {
+            get; set;
+        }
+        public string HouseNo
+        {
+            get; set;
+        }
+        public string Street
+        {
+            get; set;
+        }
+        public string Area
+        {
+            get; set;
+        }
+        public string City
+        {
+            get; set;
+        }
+        public string Pincode
+        {
+            get; set;
+        }
+        public string Username
+        {
+            get; set;
+        }
+        public string Password
+        {
+            get; set;
+        }
+    }
+}
