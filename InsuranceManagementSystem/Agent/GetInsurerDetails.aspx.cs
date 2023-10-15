73b34e4d0a3b7363c4eba0f509e4f346b1ba651e
Shloka Jaywant <shlokajaywant@gmail.com>
2023-10-15 14:50:50 +0530

    Created Stored Procedures, Separate Methods for GetInsurerDetails Module for Agent

--- InsuranceManagementSystem/Agent/GetInsurerDetails.aspx.cs
+++ InsuranceManagementSystem/Agent/GetInsurerDetails.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InsuranceManagementSystem.Agent_Classes;
+using System;
 using System.Data;
 using static InsuranceManagementSystem.Models.CommonFn;
 
@@ -7,6 +8,9 @@ namespace InsuranceManagementSystem.Agent
     public partial class GetInsurerDetails : System.Web.UI.Page
     {
         Commonfnx fn = new Commonfnx();
+        Insurer insurer = new Insurer();
+        public int InsID = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (CurrentSession.currentSession.SessionName != "Agent" || CurrentSession.currentSession.SessionID == 0)
@@ -15,54 +19,95 @@ namespace InsuranceManagementSystem.Agent
             }
         }
 
-        protected void btnGet_Click(object sender, EventArgs e)
+        protected bool GetInsurerID()
         {
             if (searchInsID.Text.Equals(string.Empty))
-                return;
-            int insID = Convert.ToInt32(searchInsID.Text.Trim());
-            DataTable dt1 = fn.Fetch("SELECT INS_ID , Ins_Name AS \"Insurer Name\", CONVERT(date, DOB) AS \"Date Of Birth\", House_Num + ', ' + Street + ', ' + Area + ', ' + City + ' - ' + CONVERT(nchar, Pincode) as \"Address\" FROM INSURER WHERE INS_ID = " + insID + ";");
-            if (dt1.Rows.Count < 1)
+                return false;
+
+            InsID = Convert.ToInt32(searchInsID.Text.Trim());
+            return true;
+        }
+
+        public bool GetInsDetails(int insID)
+        {
+            DataTable insDetailsDT = fn.Fetch("EXEC InsurerDetails @insID = " + insID + ";");
+
+            if (insDetailsDT.Rows.Count < 1)
             {
-                lblMsg.Text = "No Insurer With Entered ID Exists!";
-                lblMsg.CssClass = "alert alert-danger";
-
-                searchInsID.Text = string.Empty;
-
-                txtInsID.Text = string.Empty;
-                txtInsName.Text = string.Empty;
-                txtDOB.Text = string.Empty;
-                txtAddr.Text = string.Empty;
-
-                PhoneNumGridView.DataSource = null;
-                PhoneNumGridView.DataBind();
- 
-                BenefactorGridView.DataSource = null;
-                BenefactorGridView.DataBind();
-
-                PolicyGridView.DataSource = null;
-                PolicyGridView.DataBind();
+                ClearForm();
+                return false;
             }
             else
             {
-                txtInsID.Text = dt1.Rows[0].Field<int>(0).ToString();
-                txtInsName.Text = dt1.Rows[0].Field<string>(1);
-                txtDOB.Text = dt1.Rows[0].Field<DateTime>(2).ToLongDateString();
-                txtAddr.Text = dt1.Rows[0].Field<string>(3);
+                txtInsID.Text = insDetailsDT.Rows[0].Field<int>(0).ToString();
+                txtInsName.Text = insDetailsDT.Rows[0].Field<string>(1);
+                txtDOB.Text = insDetailsDT.Rows[0].Field<DateTime>(2).ToLongDateString();
+                txtAddr.Text = insDetailsDT.Rows[0].Field<string>(3);
+                return true;
+            }
+        }
 
-                DataTable dt2 = fn.Fetch("SELECT PhNo AS \"Phone Numbers\" FROM INSURER_PHNO WHERE INS_ID = " + insID + ";");
-                PhoneNumGridView.DataSource = dt2;
-                PhoneNumGridView.DataBind();
+        protected void FillPhoneNum(int insID)
+        {
+            DataTable phNoDT = fn.Fetch("SELECT PhNo AS \"Phone Numbers\" FROM INSURER_PHNO WHERE INS_ID = " + insID + ";");
+            PhoneNumGridView.DataSource = phNoDT;
+            PhoneNumGridView.DataBind();
+        }
 
-                DataTable dt3 = fn.Fetch("SELECT I_B.B_ID AS \"Benefactor ID\", B.B_Name AS \"Benefactor Name\", CONVERT(date, B.B_DOB) AS \"Date Of Birth\", I_B.Relationship AS \"Relationship with Insurer\" FROM INSURER_BENEFACTOR AS I_B, BENEFACTOR AS B WHERE INS_ID = " + insID + " AND I_B.B_ID = B.B_ID;");
-                BenefactorGridView.DataSource = dt3;
-                BenefactorGridView.DataBind();
+        protected void FillBenefactorTable(int insID)
+        {
+            DataTable benForInsDT = fn.Fetch("EXEC BenefactorsForInsurer @insID = " + insID + ";");
+            BenefactorGridView.DataSource = benForInsDT;
+            BenefactorGridView.DataBind();
+        }
 
-                DataTable dt4 = fn.Fetch("SELECT P_P.POL_ID AS \"Policy ID\", P.Policy_Name AS \"Policy Name\", P.Policy_Type AS \"Policy Type\", P_P.Policy_Status AS \"Policy Status\", P_P.Start_Date AS \"Policy Purchase Date\", P_P.End_Date AS \"Policy Maturity Date\", P_P_B.B_ID AS \"Benefactor ID\" FROM PURCHASED_POLICY AS P_P INNER JOIN POLICY AS P ON P_P.INS_ID = " + insID + " AND P_P.POL_ID = P.POL_ID INNER JOIN PURCHASED_POLICY_BENEFACTOR AS P_P_B ON P_P_B.INS_ID = " + insID + " AND P_P_B.POL_ID = P_P.POL_ID;");
-                PolicyGridView.DataSource = dt4;
-                PolicyGridView.DataBind();
+        protected void FillPolicyTable(int insID)
+        {
+            DataTable purPolDT = fn.Fetch("EXEC PurchasedPolicyForInsurer @insID = " + insID + ";");
+            PolicyGridView.DataSource = purPolDT;
+            PolicyGridView.DataBind();
+        }
 
-                searchInsID.Text = string.Empty;
-            } 
+        protected void ClearForm()
+        {
+            lblMsg.Text = "No Insurer With Entered ID Exists!";
+            lblMsg.CssClass = "alert alert-danger";
+
+            searchInsID.Text = string.Empty;
+
+            txtInsID.Text = string.Empty;
+            txtInsName.Text = string.Empty;
+            txtDOB.Text = string.Empty;
+            txtAddr.Text = string.Empty;
+
+            PhoneNumGridView.DataSource = null;
+            PhoneNumGridView.DataBind();
+
+            BenefactorGridView.DataSource = null;
+            BenefactorGridView.DataBind();
+
+            PolicyGridView.DataSource = null;
+            PolicyGridView.DataBind();
+        }
+
+        protected void btnGet_Click(object sender, EventArgs e)
+        {
+            if (!GetInsurerID())
+            {
+                ClearForm();
+                return;
+            }
+
+            bool insSuccess = GetInsDetails(InsID);
+            if(!insSuccess)
+            {
+                ClearForm();
+                return;
+            }
+
+            FillPhoneNum(InsID);
+            FillBenefactorTable(InsID);
+            FillPolicyTable(InsID);
         }
     }
 }
